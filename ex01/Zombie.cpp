2c1b6b064b79487e0d49774e1781ab703818d2a7
Hajar Charkaoui <hacharka@c1r2p5.1337.ma>
2026-02-11 15:50:25 +0100

    test project

--- ex01/Zombie.cpp
+++ ex01/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: hacharka <hacharka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 14:51:20 by hacharka          #+#    #+#             */
-/*   Updated: 2026/02/07 18:16:26 by hacharka         ###   ########.fr       */
+/*   Updated: 2026/02/11 15:46:14 by hacharka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,19 @@ std::string	Zombie::getName() const
 	return name;
 };
 
+Zombie::Zombie() : name("") {};
+Zombie::Zombie(std::string name) : name (name) {};
+
 void	Zombie::setName(std::string name)
 {
 	this->name = name;
 }
 
-Zombie::Zombie() : name ("hajar") {};
 void	Zombie::announce()
 {
-	std::cout << this->name << " : BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << name << " : BraiiiiiiinnnzzzZ..." << std::endl;
 }
 Zombie::~Zombie()
 {
-	std::cout << "The zombie " << this->name << " was destroyed!" << std::endl;
+	std::cout << "The zombie " << name << " was destroyed!" << std::endl;
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,14 +6,19 @@
 /*   By: hacharka <hacharka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 14:51:29 by hacharka          #+#    #+#             */
-/*   Updated: 2026/02/07 19:42:08 by hacharka         ###   ########.fr       */
+/*   Updated: 2026/02/11 15:48:29 by hacharka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.h"
 
+void	ll()
+{
+	system("leaks -q Moarbrainz");
+}
 int	main()
 {
+	atexit(ll);
 	Zombie *array;
 
 	array = zombieHorde(10, "hajar");
